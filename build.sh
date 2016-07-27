bff8cba97d9315274b050b29a3263a93bda5d305
davemackintosh <me@davemackintosh.co.uk>
2016-07-27 11:52:23 +0100

    Modifications for the docs folder.
    Signed-off-by: davemackintosh <me@davemackintosh.co.uk>

--- build.sh
+++ build.sh
@@ -1,26 +1,40 @@
 #!/bin/bash
 
-# Loop over each found markdown file.
-for file in content/*.md; do
-  # Check it's an actual readable file.
-  if [ -e "$file" ] ; then
-    # Get our 💩 together.
-    TEMPLATE=`cat template.html`
-    CONTENT=`cat "$file" | pandoc --no-highlight -f markdown`
-
-    # Compile the template.
-    FILE="${TEMPLATE/CONTENT_HERE/$CONTENT}"
-
-    # Pandoc just puts the language in there, we want language-{language}.
-    FILE="${FILE/class=\"javascript\"/class=\"language-javascript\"}"
-
-    # Get the base name.
-    WRITE_TO=$(basename $file | cut -d. -f1)
-
-    # Create the path for it.
-    mkdir -p $WRITE_TO
-
-    # Write the file contents like a baws.
-    echo "$FILE" > "./$WRITE_TO/index.html"
+loop_templates () {
+  if [ -z "$1" ] ; then
+    echo "Incorrect number of arguments to loop_templates <glob pattern>."
+    exit 1
   fi
-done
+
+  # Loop over each found markdown file.
+  for file in ${1}; do
+    # Check it's an actual readable file.
+    if [ -e "$file" ] ; then
+      # Get our 💩 together.
+      TEMPLATE=`cat template.html`
+      CONTENT=`cat "$file" | pandoc --no-highlight -f markdown`
+
+      # Compile the template.
+      FILE="${TEMPLATE/CONTENT_HERE/$CONTENT}"
+
+      # Pandoc just puts the language in there, we want language-{language}.
+      FILE="${FILE/class=\"javascript\"/class=\"language-javascript\"}"
+
+      # If we passed in a target directory, use that.
+      if [ $# == 2 ] ; then
+        WRITE_TO="$2$(basename $file | cut -d. -f1 | tr '[:upper:]' '[:lower:]')"
+      else
+        WRITE_TO=$(echo $file | cut -d. -f1 | sed 's,^[^/]*/,,' | tr '[:upper:]' '[:lower:]')
+      fi
+
+      # Create the path for it.
+      mkdir -p "$WRITE_TO"
+
+      # Write the file contents like a baws.
+      echo "$FILE" > "./$WRITE_TO/index.html"
+    fi
+  done
+}
+
+loop_templates "content/*.md"
+loop_templates "content/docs/*.md" docs/0.4.0/
