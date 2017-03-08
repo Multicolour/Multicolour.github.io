52064aa165a23f1081f5e9a678ac93eaa04654a6
davemackintosh <me@davemackintosh.co.uk>
2017-03-08 10:25:41 +0000

    Mobile friendly aside and improved search functionality

--- _site/js/main.js
+++ _site/js/main.js
@@ -19,19 +19,39 @@
 
   // Try to find the search box.
   var search = document.querySelector(".search input[type=search]")
-  var filterable = Array.from(document.querySelectorAll("li[data-keywords]"))
+  var search_results = document.querySelector(".search-results")
+  var filterable = Array.from(search_results.querySelectorAll("li[data-keywords]"))
 
   if (search) {
+    const pos = search.getBoundingClientRect()
+
+    search.addEventListener("focus", function() {
+      search_results.classList.add("intent")
+      search_results.style.top = pos.bottom + "px"
+    })
+    search.addEventListener("blur", function() {
+      setTimeout(function() {
+        search_results.classList.remove("intent")
+        search_results.classList.remove("searched")
+        search_results.style.top = null
+      }, 300)
+    })
     search.addEventListener("keyup", function(event) {
       var value = event.target.value.toLowerCase()
-      var visibility = value.length === 0 ? "" : "none"
+
+      if (value.length > 0)
+        search_results.classList.add("searched")
+      else
+        search_results.classList.remove("searched")
 
       filterable.forEach(function(element) {
         var search = (element.dataset.keywords + " " + element.innerText).toLowerCase()
-        if (search.search(value) <= 0)
-          element.style.display = visibility
+        if (value.length === 0)
+          element.classList.add("visible")
+        else if (value.length > 0 && search.search(value) > -1)
+          element.classList.add("visible")
         else
-          element.style.display = ""
+          element.classList.remove("visible")
       })
     })
   }
